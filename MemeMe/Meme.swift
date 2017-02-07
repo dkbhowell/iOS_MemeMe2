8e1050ddc842cdd586d1d61858deaa107171c036
Dustin Howell <dkbhowell@gmail.com>
2017-02-06 16:43:41 -0800

    mostly complete '1.0  ' and project/file renaming ;

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMeSandBox
+//
+//  Created by Dustin Howell on 2/6/17.
+//  Copyright © 2017 Dustin Howell. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    let topText: String
+    let bottomText: String
+    let originalImage: UIImage
+    let memedImage: UIImage
+}
---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,217 @@
+//
+//  ViewController.swift
+//  MemeMeSandBox
+//
+//  Created by Dustin Howell on 2/2/17.
+//  Copyright © 2017 Dustin Howell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topMemeText: UITextField!
+    @IBOutlet weak var bottomMemeText: UITextField!
+    @IBOutlet weak var bottomToolbar: UIToolbar!
+    
+    
+    
+    let memeTextAttributes: [String:Any] = [
+        NSStrokeColorAttributeName: UIColor.black,
+        NSForegroundColorAttributeName: UIColor.white,
+        NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40) as Any,
+        NSStrokeWidthAttributeName: -3.0
+    ]
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        topMemeText.delegate = self
+        bottomMemeText.delegate = self
+        topMemeText.defaultTextAttributes = memeTextAttributes
+        bottomMemeText.defaultTextAttributes = memeTextAttributes
+        topMemeText.textAlignment = .center
+        bottomMemeText.textAlignment = .center
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeToKeyboardNotifications()
+    }
+    
+    
+    // MARK: Actions
+    @IBAction func pickImage(_ sender: UIBarButtonItem) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        self.present(pickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func takePicture(_ sender: UIBarButtonItem) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .camera
+        self.present(pickerController, animated: true, completion: nil)
+
+    }
+    
+    @IBAction func shareMeme(_ sender: UIBarButtonItem) {
+        let memeImage = generateMemedImage()
+        
+        let shareController = UIActivityViewController(activityItems: [memeImage], applicationActivities: nil)
+        
+        shareController.completionWithItemsHandler = {
+            activity, success, items, error in
+            _ = Meme(topText: self.topMemeText.text!, bottomText: self.bottomMemeText.text!, originalImage: self.imageView.image!, memedImage: memeImage)
+        }
+        
+        self.present(shareController, animated: true, completion: nil)
+    }
+    
+    @IBAction func resetUI(_ sender: UIBarButtonItem) {
+        imageView.image = UIImage(named: "default_image")
+        topMemeText.text = "TOP"
+        bottomMemeText.text = "BOTTOM"
+    }
+    
+    
+    
+    // Image Picker Delegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("image picker picked")
+        
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            imageView.image = image
+            for constraint in imageView.constraints {
+                print(constraint)
+            }
+        }
+        
+        dismiss(animated: true, completion: nil)
+        return
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("image picker cancel")
+        dismiss(animated: true, completion: nil)
+        return
+    }
+    
+    // Text Field Delegate
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        let currentText = textField.text
+        if currentText == "TOP" || currentText == "BOTTOM" {
+            textField.text = ""
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // Notification methods
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeToKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow , object: nil)
+    }
+    
+    func keyboardWillShow(_ notification: Notification) {
+        view.frame.origin.y = 0 - getKeyboardHeight(notification)
+    }
+    
+    func keyboardWillHide(_ notification: Notification) {
+        view.frame.origin.y = 0
+    }
+    
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat {
+        
+        // detect if the bottom text field is being edited
+        if bottomMemeText.isEditing {
+            let userInfo = notification.userInfo!
+            let keyboardSize = userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue
+            return keyboardSize.cgRectValue.height
+        }
+        
+        // do not move view up if the bottom text field is not being edited
+        return 0
+    }
+    
+    // MARK: Utility Functions
+    
+    func generateMemedImage() -> UIImage {
+        // hide toolbar on bottom
+        bottomToolbar.isHidden = true
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        
+        // capture the memed image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        // show toolbar
+        bottomToolbar.isHidden = false
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+        return memedImage
+    }
+    
+    func cropImageViewToImageSize(_ imageView: UIImageView) {
+        imageView.translatesAutoresizingMaskIntoConstraints = true
+        if let imageSize = getImageSize(for: imageView) {
+            imageView.frame = imageSize
+        }
+    }
+    
+    func getImageSize(for imageView: UIImageView) -> CGRect? {
+        guard let image = imageView.image else {
+            print("No image in imageview")
+            return nil
+        }
+        
+        let imageRatio = image.size.width / image.size.height
+        let viewRatio = imageView.frame.size.width / imageView.frame.size.height
+        
+        if imageRatio < viewRatio {
+            let scale = imageView.frame.size.height / image.size.height
+            let width = scale * image.size.width
+            let topLeftX = (imageView.frame.size.width - width) * 0.5
+            return CGRect(x: topLeftX, y: 0, width: width, height: imageView.frame.size.height)
+        } else {
+            let scale = imageView.frame.size.width / image.size.width
+            let height = scale * image.size.height
+            let topLeftY = (imageView.frame.size.height - height) * 0.5
+            return CGRect(x: 0, y: topLeftY, width: imageView.frame.size.width, height: height)
+        }
+    }
+    
+    func logDimens(for imageView: UIImageView) {
+        let width = imageView.frame.size.width
+        let height = imageView.frame.size.height
+        let dimenString = "Width: \(width) \nHeight: \(height)"
+        print(dimenString)
+    }
+}
+
+
+
+
+
+
